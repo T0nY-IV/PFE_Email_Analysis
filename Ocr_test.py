6054f0c8575a953b4b83801218aaca57c7a3d8ce
YESSIN ht <Yessineht@gmail.com>
2026-03-16 08:46:05 +0100

    changed the ocr of photos from using glm-ocr to tesseract and pytesseract (the python library for tesseract)

---
+++ Ocr_test.py
@@ -0,0 +1,8 @@
+from Ocr_methodes import Ocr_pdf, Ocr_picture, Ocr_pdf_Init
+
+layout = Ocr_pdf_Init()
+pdf_content = Ocr_pdf("C:/Users/loq/Downloads/emails_output/attachments/9576/Rapport.pdf", layout)
+print(f"{pdf_content._.markdown}")
+print("\n\n-----------------------------------\n\n")
+pic_content = Ocr_picture("D:/studying/3eme info(DSI33)/PFE/emails_output/images/9833/Asset11.png")
+print(f"{pic_content}")
